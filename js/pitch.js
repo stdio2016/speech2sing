ce3dc415cca8b16c648819072007354b080c685a
stdio2016 <randomfrank8@gmail.com>
2018-08-16 17:42:41 +0800

    pitch detection by FFT

---
+++ js/pitch.js
@@ -0,0 +1,32 @@
+var fftSize = 2048;
+function analyzePitch(buf, smpRate) {
+  var i;
+  var wind = new Float32Array(fftSize * 2);
+  var amps = new Float32Array(fftSize/2);
+  var ans = [];
+  for (i = fftSize; i < buf.length; i += fftSize) {
+    var j = 0;
+    for (j = 0; j < fftSize; j++) {
+      wind[j*2] = buf[i - fftSize + j];
+    }
+    var result = stdio2017.FFT.transform(wind, true);
+    for (j = 0; j < fftSize/2; j++) {
+      var re = result[j*2];
+      var im = result[j*2+1];
+      amps[j] = re*re + im*im;
+    }
+    var max = 0, freq = 0;
+    for (j = Math.floor(fftSize/smpRate * 80); j < fftSize/smpRate * 500; j++) {
+      var smp2 = (amps[j*2] + amps[j*2+1]) / 2;
+      var smp3 = (amps[j*3] + amps[j*3+1] + amps[j*3+2]) / 3;
+      var smp4 = (amps[j*4] + amps[j*4+1] + amps[j*4+2]) / 3;
+      var totle = (amps[j] + smp2 + smp3 + smp4) / Math.sqrt(j);
+      if (totle > max) {
+        max = totle;
+        freq = j;
+      }
+    }
+    ans.push([i/smpRate, freq * smpRate/fftSize]);
+  }
+  return ans;
+}
---
+++ js/pitch_ui.js
@@ -0,0 +1,108 @@
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var clips = document.querySelector('.sound-clips');
+function addClipInterface(name) {
+  var clip = new Option(name, "r_"+name);
+  clips.appendChild(clip);
+  console.log("added " + name);
+}
+
+function startup() {
+  getSoundNames().then(function (names) {
+    clips.options.remove("loading");
+    names.forEach(addClipInterface);
+    if (names.length === 0) {
+      var no = new Option("No clips ;-(", "");
+      clips.options.add(no);
+      clips.disabled = true;
+      btnAnalyzeClip.disabled = true;
+    }
+  })['catch'](function (x) {
+    console.error(x);
+  });
+}
+
+function analyzeClip() {
+  resumeContext();
+  var name = clips.value;
+  if (name.substr(0,2) === "r_") {
+    name = name.substr(2);
+    getSound(name).then(function (file) {
+      analyzeFile(file);
+    }).catch(function () {
+      console.error("Clip \""+name+"\" not found");
+    });
+  }
+}
+
+function analyzeFile(file) {
+  resumeContext();
+  if (!file) {
+    alert("Please choose a file");
+    return;
+  }
+  var fr = new FileReader();
+  fr.onload = function () {
+    audioCtx.decodeAudioData(fr.result, getBuffer, error);
+  };
+  fr.readAsArrayBuffer(file);
+  function getBuffer(audioBuf) {
+    var buf = audioBuf.getChannelData(0);
+    var ans = analyzePitch(buf, audioCtx.sampleRate);
+    //var snd = audioCtx.createBufferSource();
+    var snd = audioCtx.createOscillator();
+    var gain = audioCtx.createGain();
+    snd.buffer = buf;
+    var timbreRe = new Float32Array([0.5, 0.4, 0.3, 0.2, 0.1]);
+    var timbreIm = new Float32Array([0, 0, 0, 0, 0]);
+    var timbre = audioCtx.createPeriodicWave(timbreRe, timbreIm);
+    snd.setPeriodicWave(timbre);
+    snd.connect(gain);
+    gain.gain.value = 0.4;
+    gain.connect(audioCtx.destination);
+    (snd.start || snd.noteOn).call(snd);
+    ans.forEach(function (pitch) {
+      snd.frequency.setValueAtTime(pitch[1], audioCtx.currentTime + pitch[0]);
+    });
+    console.log(buf.length);
+    var dur = buf.length / audioCtx.sampleRate;
+    (snd.stop || snd.noteOff).call(snd, audioCtx.currentTime + dur);
+  }
+  function error(x) {
+    switch (x.name) {
+      case "EncodingError":
+        alert("File is not an audio, or the format is not supported");
+        break;
+      default:
+        alert(x);
+    }
+  }
+}
+
+function saveToBrowser(file) {
+  if (!file) {
+    alert("Please choose a file");
+    return;
+  }
+  var name = prompt("Clip name:");
+  if (name) {
+    saveSound(name, file);
+  }
+}
+
+function resumeContext() {
+  if (audioCtx && audioCtx.state === "suspended") {
+    audioCtx.resume();
+    var r = audioCtx.createOscillator();
+    (r.start || r.noteOn).call(r);
+    (r.stop || r.noteOff).call(r, audioCtx.currentTime+Math.random()*0.5);
+    r.frequency.value = 880;
+    r.detune.value = Math.random()*100 - 50;
+    var s = audioCtx.createGain();
+    s.gain.value = Math.random() * 0.5;
+    r.connect(s);
+    s.connect(audioCtx.destination);
+  }
+}
+
+addEventListener('click', resumeContext);
+addEventListener('touchstart', resumeContext);
