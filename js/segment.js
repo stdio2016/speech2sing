a68268f6c990f6c127d1c2b14ab4c24a829e07b7
stdio2016 <randomfrank8@gmail.com>
2019-01-01 15:30:51 +0800

    select range

--- js/segment.js
+++ js/segment.js
@@ -1,7 +1,7 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var clips = document.querySelector('.sound-clips');
 var canvas = document.querySelector('#canvas');
-var soundBuffer;
+var soundBuffer = {sampleRate: audioCtx.sampleRate};
 var waveArray = [[]];
 var MaxZoomLevel = 12;
 var zoomLevel = MaxZoomLevel;
@@ -10,6 +10,9 @@ var zoomPanV = 0;
 var yAxisZoom = 1;
 var playStartTime = 0;
 var playingSound = false;
+var selected = {start: 0, end: 0.5};
+var segments = {};
+var segmentId = 0;
 
 function addClipInterface(name) {
   name = name.name;
@@ -104,7 +107,7 @@ function buildWaveArray() {
 }
 
 requestAnimationFrame(showWave);
-var mouse = {x: 0, y: 0, vx: 0, px: 0, dragging: false, t: 0};
+var mouse = {x: 0, y: 0, vx: 0, px: 0, dragging: false, t: 0, dragBar: 0};
 var visualizeCallbackId = 0;
 
 function fixZoomRange() {
@@ -112,6 +115,22 @@ function fixZoomRange() {
   if (zoomLevel < 0) zoomLevel = 0;
   zoomPan = Math.min(zoomPan, waveArray[zoomLevel].length/2 - canvas.width);
   if (zoomPan < 0) zoomPan = 0;
+}
+
+function fixSelectRange(final) {
+  if (final && selected.start > selected.end) {
+    var tmp = selected.start;
+    selected.start = selected.end;
+    selected.end = tmp;
+  }
+  var len = waveArray[0].length / soundBuffer.sampleRate;
+  selected.start = Math.min(Math.max(0, selected.start), len);
+  selected.end = Math.min(Math.max(0, selected.end), len);
+}
+
+function timeToCanvasPos(t) {
+  var smpRate = soundBuffer ? soundBuffer.sampleRate : audioCtx.sampleRate;
+  return t * smpRate / Math.pow(2, zoomLevel+1) - zoomPan;
 }
 
 function showWave() {
@@ -135,7 +154,8 @@ function showWave() {
   fixZoomRange();
   var wav = waveArray[zoomLevel];
   
-  ctx.strokeStyle = '#0f0';
+  ctx.globalCompositeOperation = 'lighter';
+  ctx.strokeStyle = '#080';
   ctx.beginPath();
   ctx.moveTo(0, height/2);
   for (var i = 0; i < width; i++) {
@@ -146,13 +166,24 @@ function showWave() {
     ctx.lineTo(x, (1 - wav[j*2+1] * yAxisZoom) * height/2);
   }
   ctx.stroke();
+  if (selected) {
+    var x1 = timeToCanvasPos(selected.start);
+    var x2 = timeToCanvasPos(selected.end);
+    ctx.fillStyle = '#044';
+    ctx.fillRect(x1, 0, x2 - x1, height);
+  }
   if (playingSound) {
     var t = audioCtx.currentTime - playStartTime;
-    var x = t * soundBuffer.sampleRate / Math.pow(2, zoomLevel+1) - zoomPan;
+    var x = timeToCanvasPos(t);
     ctx.fillStyle = '#f0f';
-    ctx.beginPath();
-    ctx.rect(x - 2, 0, 4, height);
-    ctx.fill();
+    ctx.fillRect(x - 2, 0, 4, height);
+  }
+  ctx.globalCompositeOperation = 'source-over';
+  if (selected) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(x1-2, 0, 4, height);
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(x2-2, 0, 4, height);
   }
 }
 
@@ -168,40 +199,57 @@ function zoomOut() {
     zoomPan >>= 1;
   }
 }
-function playRange() {
-  if (!soundBuffer) return ;
+function playRange(from, duration) {
+  if (!soundBuffer || duration <= 0) return ;
+  if (playingSound) {
+    playingSound.onended = function () {};
+    playingSound.stop();
+  }
   fixZoomRange();
-  var du = Math.pow(2, zoomLevel+1) / soundBuffer.sampleRate;
-  var t = zoomPan * du;
   var node = audioCtx.createBufferSource();
   node.buffer = soundBuffer;
   node.connect(audioCtx.destination);
   
   var start = audioCtx.currentTime;
-  playStartTime = start - t;
-  playingSound = true;
-  node.start(start, t, du * canvas.width);
+  playStartTime = start - from;
+  playingSound = node;
+  node.start(start, from, duration);
   node.onended = function () {
-    playingSound = false;
+    playingSound = null;
   };
+}
+
+function playSelected() {
+  if (selected) playRange(selected.start, selected.end - selected.start);
+}
+function playVisible() {
+  if (!soundBuffer) return ;
+  var unit = Math.pow(2, zoomLevel+1) / soundBuffer.sampleRate;
+  playRange(zoomPan * unit, canvas.width * unit);
 }
 
 function canvasMouseDown(e){
   e.preventDefault();
   if (e.button == 0) {
-    var newx = e.clientX;
+    var newx = e.offsetX;
     mouse.x = newx;
     mouse.vx = 0;
     mouse.px = newx;
-    mouse.dragging = true;
+    if (Math.abs(newx - timeToCanvasPos(selected.start)) < 10) {
+      mouse.dragBar = 1;
+    }
+    else if (Math.abs(newx - timeToCanvasPos(selected.end)) < 10) {
+      mouse.dragBar = 2;
+    }
+    else mouse.dragging = true;
     mouse.t = e.timeStamp;
   }
 }
 
 function canvasMouseMove(e){
   e.preventDefault();
-  var newx = e.clientX;
-  if (e.buttons & 1 && mouse.dragging) {
+  var newx = e.offsetX;
+  if ((e.buttons & 1) && mouse.dragging) {
     zoomPan -= (newx - mouse.x);
     mouse.x = newx;
     var t = e.timeStamp;
@@ -210,6 +258,14 @@ function canvasMouseMove(e){
     mouse.vx = newpx - mouse.px;
     mouse.px = newpx;
     mouse.t = t;
+  }
+  else if ((e.buttons & 1) && mouse.dragBar) {
+    var unit = Math.pow(2, zoomLevel+1) / soundBuffer.sampleRate;
+    if (mouse.dragBar == 1)
+      selected.start += (newx - mouse.x) * unit;
+    if (mouse.dragBar == 2)
+      selected.end += (newx - mouse.x) * unit;
+    fixSelectRange();
   }
   mouse.x = newx;
 }
@@ -220,6 +276,7 @@ function canvasMouseUp(e){
     else zoomPanV = 0;
     mouse.dragging = false;
   }
+  fixSelectRange(true);
 }
 canvas.addEventListener('mousedown', canvasMouseDown);
 canvas.addEventListener('mousemove', canvasMouseMove);
@@ -241,8 +298,8 @@ function canvasTouchStart(e) {
     timeStamp: e.timeStamp,
     preventDefault: ignore,
     button: 0,
-    clientX: touch[0].clientX,
-    clientY: touch[0].clientY
+    offsetX: touch[0].offsetX,
+    offsetY: touch[0].offsetY
   });
 }
 function canvasTouchMove(e) {
@@ -253,10 +310,9 @@ function canvasTouchMove(e) {
     timeStamp: e.timeStamp,
     preventDefault: ignore,
     buttons: 1,
-    clientX: touch[0].clientX,
-    clientY: touch[0].clientY
+    offsetX: touch[0].offsetX,
+    offsetY: touch[0].offsetY
   });
-  console.log(e.timeStamp, mouse.vx);
 }
 function canvasTouchEnd(e) {
   e.preventDefault();
